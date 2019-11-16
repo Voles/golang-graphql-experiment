000b56b8a2cbc66695cf8ada15d54cb2560644dd
Niels Dequeker <niels@newfront.eu>
2019-11-16 14:09:45 +0100

    Setup Graphiql

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Voles/golang-graphql-experiment
 
 go 1.13
+
+require (
+	github.com/friendsofgo/graphiql v0.2.2
+	github.com/graphql-go/graphql v0.7.8
+)
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/friendsofgo/graphiql"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	graphiqlHandler, err := graphiql.NewGraphiqlHandler("/graphql")
+	if err != nil {
+		panic(err)
+	}
+
+	//http.Handle("/graphql", gqlHandler())
+	http.Handle("/graphiql", graphiqlHandler)
+	http.ListenAndServe("0.0.0.0:8080", nil)
+
 }
