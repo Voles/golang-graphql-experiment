a88941df723f7381ec5f7d945b1afcf126c2f4d5
Niels Dequeker <niels@newfront.eu>
2019-11-16 15:10:44 +0100

    Implement name and objectByID fields

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# golang-graphql-experiment
+# Golang GraphQL experiment
+
 🧪
+
+```shell script
+go get
+go run .
+```
+
+## Example queries
+
+**Get something**
+```text
+http://localhost:8080/graphql?query=%0A%7B%0A%20%20name%0A%7D&variables=%7B%0A%0A%7D
+```
+
+**Get something by ID**
+```text
+http://localhost:8080/graphql?query=%0A%7B%0A%20%20objectByID(id%3A%2042)%20%7B%0A%20%20%20%20id%0A%20%20%20%20name%0A%20%20%7D%0A%7D&variables=%7B%0A%0A%7D
+```
+
--- main.go
+++ main.go
@@ -1,19 +1,82 @@
 package main
 
 import (
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
+	"log"
 	"net/http"
+)
 
-	"github.com/friendsofgo/graphiql"
+var objectType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Object",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.ID,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},
 )
 
 func main() {
-	graphiqlHandler, err := graphiql.NewGraphiqlHandler("/graphql")
-	if err != nil {
-		panic(err)
+	type Object struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
 	}
 
-	//http.Handle("/graphql", gqlHandler())
-	http.Handle("/graphiql", graphiqlHandler)
-	http.ListenAndServe("0.0.0.0:8080", nil)
+	objects := map[string]Object{
+		"42": {
+			ID:   "42",
+			Name: "John Doe",
+		},
+	}
 
+	fields := graphql.Fields{
+		"name": &graphql.Field{
+			Type:        graphql.String,
+			Description: "Retrieve the name of a random fictional animal",
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "Marsupilami 🐒", nil
+			},
+		},
+		"objectByID": &graphql.Field{
+			Type: objectType,
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.ID,
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				idQuery, isOK := p.Args["id"].(string)
+				if isOK {
+					return objects[idQuery], nil
+				}
+				return nil, nil
+			},
+			Description: "Get an object by its ID",
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	graphqlHandler := handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
+	http.Handle("/graphql", graphqlHandler)
+	err = http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatalf("failed to listen and serve, error: %v", err)
+	}
 }
